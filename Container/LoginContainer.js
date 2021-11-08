c7b5910b313abbdb5e129a942aa6683b2cd90d10
shaggyme <alishaan089@gmail.com>
2021-11-08 10:57:13 +0530

    Add files via upload

---
+++ Container/LoginContainer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Login from "../Components/Login/Login";
+import Footer from "../Components/Footer/Footer";
+
+const LoginContainer = ()=>{
+    return (
+        <div>
+            <Login/>
+            <Footer/>
+        </div>
+    )
+}
+
+export default LoginContainer
